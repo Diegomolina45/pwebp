66e8d6804553fd74194d27c8e565b40add1b7ca0
Diego Molina <molina542001@gmail.com>
2021-06-14 00:29:44 -0500

    portafolio web

---
+++ js/mainP.js
@@ -0,0 +1,14 @@
+var botonHambCerrar = document.getElementById('botonHambCerrar');
+var menuResponsive = document.getElementById('menuResponsive');
+var botonNav = document.getElementById('botonNav');
+
+botonHambCerrar.onclick = function () {
+    menuResponsive.style.right = "-500px";
+}
+
+botonNav.onclick = function () {
+    menuResponsive.style.right = "0";
+}
+
+//aqui comienza el slider de los videos");
+
